e39ef44161f99456a541e0473c4e40419d426550
some1shell <246136186+some1shell@users.noreply.github.com>
2026-05-21 23:07:22 +0200

    Remove prints

--- llm_calls.py
+++ llm_calls.py
@@ -81,15 +81,12 @@ def parse_gemini(model_name: str, user_prompt: str, response_format, system_prom
 
 
 def parse_llm(llm_provider, llm_model, user_prompt: str, response_format, system_prompt=None, max_tokens: int = 2000):
-    print('in parse_llm, provider = ', llm_provider)
     match llm_provider:
         case "gemini":
             return parse_gemini(llm_model, user_prompt, response_format, system_prompt, max_tokens)
         case "mistral":
             return parse_mistral(llm_model, user_prompt, response_format, system_prompt, max_tokens)
         case "openrouter":
-            print('llm_provider = ', llm_provider)
-            print('openrouter_client = ', openrouter_client)
             return parse_openrouter(llm_model, user_prompt, response_format, system_prompt, max_tokens)
         case _:
             raise ValueError(f"Unknown LLM_PROVIDER: {llm_provider!r}")
